b597a2ea1f11206316a8dc974c650be605dbf906
Man0029 <manteo2005pro@gmail.com>
2025-05-23 22:40:28 +0700

    CoffeeShopS1S2

---
+++ CoffeeShop-S2/CoffeeShop/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using CoffeeShop.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public IActionResult Shop()
+        {
+            return View(productRepository.GetAllProducts());
+        }
+    }
+}
